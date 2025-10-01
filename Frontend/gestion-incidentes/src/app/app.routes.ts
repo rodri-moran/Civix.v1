c8de6d24368a8fdd54f949e4097ad741eed9e2a2
Rodri-Notebook2 <rodrigomoran143@gmail.com>
2025-10-01 20:28:24 -0300

    Corrigiendo error de rutas

--- Frontend/gestion-incidentes/src/app/app.routes.ts
+++ Frontend/gestion-incidentes/src/app/app.routes.ts
@@ -1,15 +1,23 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login';
-import { Register } from './components/register/register';
-import { CitizenDashboard } from './components/dashboards/citizen-dashboard';
-import { ReportarIncidente } from './components/reportIncident/reportar-incidente';
-import { MisReportes } from './components/misReportes/mis-reportes';
-import { AuthGuard } from './guards/auth-guard';
-
 export const routes: Routes = [
-  { path: 'login', loadComponent: () => import('./components/login/login').then(m => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import('./components/register/register').then(m => m.Register) },
-  { path: 'mis-reportes', loadComponent: () => import('./components/misReportes/mis-reportes').then(m => m.MisReportes), canActivate: [AuthGuard] },
-  { path: 'reportar', loadComponent: () => import('./components/reportIncident/reportar-incidente').then(m => m.ReportarIncidente), canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { 
+    path: 'login', 
+    loadComponent: () => import('./components/login/login').then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./components/register/register').then(m => m.Register) 
+  },
+  { 
+    path: 'mis-reportes', 
+    loadComponent: () => import('./components/misReportes/mis-reportes').then(m => m.MisReportes) 
+  },
+  { 
+    path: 'reportar-incidente', 
+    loadComponent: () => import('./components/reportIncident/reportar-incidente').then(m => m.ReportarIncidente)
+  },
+  { 
+    path: 'citizen-dashboard', 
+    loadComponent: () => import('./components/dashboards/citizen-dashboard').then(m => m.CitizenDashboard)
+  }
 ];
